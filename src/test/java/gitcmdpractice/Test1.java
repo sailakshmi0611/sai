0593e518290fe0db23959ce34705558a20314956
sailakshmi0611 <sailakshmikasi05@gmail.com>
2023-09-29 12:40:57 +0530

    my first commit

---
+++ src/test/java/gitcmdpractice/Test1.java
@@ -0,0 +1,23 @@
+package gitcmdpractice;
+
+import java.util.Scanner;
+
+public class Test1 {
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String str="";
+		System.out.println("Enter string name");
+		str=sc.nextLine();
+		String reverse ="";
+		for(int i=str.length()-1;i>=0;i--)
+		{
+		 reverse=reverse+str.charAt(i);
+		}
+		System.out.println("Reverse string is "+"\t"+reverse);
+	}
+
+	}
+
+
